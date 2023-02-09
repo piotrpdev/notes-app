d00bcd4dcad75f70605e64c5764a86f3318e7a4a
piotrpdev <peterplaczek03@gmail.com>
2023-02-09 15:36:54 +0000

    skeleton for notes app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun mainMenu() : Int {
+    print(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+    return scanner.nextInt()
+}
+
+fun addNote(){
+    println("You chose Add Note")
+}
+
+fun listNotes(){
+    println("You chose List Notes")
+}
+
+fun updateNote(){
+    println("You chose Update Note")
+}
+
+fun deleteNote(){
+    println("You chose Delete Note")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exitProcess(0)
+}
+
+fun runMenu() {
+    do {
+        when (val option = mainMenu()) {
+            1  -> addNote()
+            2  -> listNotes()
+            3  -> updateNote()
+            4  -> deleteNote()
+            0  -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
+}
+
+fun main(args: Array<String>) {
+    runMenu()
 }
