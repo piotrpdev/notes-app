444ce1c9ba085b202fec7b254edd97b3404fa20f
Piotr Płaczek <99439005+piotrpdev@users.noreply.github.com>
2023-02-16 15:54:30 +0000

    Note data class

---
+++ src/main/kotlin/models/Note.kt
@@ -0,0 +1,7 @@
+package models
+
+data class Note(val noteTitle: String,
+                val notePriority: Int,
+                val noteCategory: String,
+                val isNoteArchived :Boolean){
+}
